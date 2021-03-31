d1ecead462ab56019995c4571d8b995ef9dbf370
zaidalyafeai <alyafey22@gmail.com>
2021-03-31 12:52:51 +0300

    first commit

---
+++ dialect_speech_corpus/dialect_speech_corpus.py
@@ -0,0 +1,107 @@
+"""Arabic Speech Corpus"""
+
+from __future__ import absolute_import, division, print_function
+
+import os
+
+import datasets
+import soundfile as sf 
+
+_CITATION = """
+"""
+
+_DESCRIPTION = """\
+
+
+```python
+import soundfile as sf
+
+def map_to_array(batch):
+    speech_array, _ = sf.read(batch["file"])
+    batch["speech"] = speech_array
+    return batch
+
+dataset = dataset.map(map_to_array, remove_columns=["file"])
+```
+"""
+
+_URL = "mgb3.zip"
+
+import soundfile as sf
+
+class DialectSpeechCorpusConfig(datasets.BuilderConfig):
+    """BuilderConfig for DialectSpeechCorpusCorpus."""
+
+    def __init__(self, **kwargs):
+        """
+        Args:
+          data_dir: `string`, the path to the folder containing the files in the
+            downloaded .tar
+          citation: `string`, citation for the data set
+          url: `string`, url for information about the data set
+          **kwargs: keyword arguments forwarded to super.
+        """
+        super(DialectSpeechCorpusConfig, self).__init__(version=datasets.Version("2.1.0", ""), **kwargs)
+
+
+def map_to_array(batch):
+    start, stop = batch['segment'].split('_')
+    speech_array, _ = sf.read(batch["file"], start = start, stop = stop)
+    batch["speech"] = speech_array
+    return batch
+
+class DialectSpeechCorpus(datasets.GeneratorBasedBuilder):
+    """DialectSpeechCorpus dataset."""
+
+    BUILDER_CONFIGS = [
+        DialectSpeechCorpusConfig(name="clean", description="'Clean' speech."),
+    ]
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "file": datasets.Value("string"),
+                    "label": datasets.features.ClassLabel(
+                        names=[
+                        'EGY',
+                        'NOR',
+                        'GLF',
+                        'LAV',
+                        'MSA'
+                        ]
+                    )
+                }
+            ),
+            supervised_keys=("file", "text"),
+            homepage=_URL,
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        archive_path = '/workspace/data_dir/'
+        return [
+            datasets.SplitGenerator(name="train", gen_kwargs={"archive_path": os.path.join(archive_path, "train")}),
+            datasets.SplitGenerator(name="dev", gen_kwargs={"archive_path": os.path.join(archive_path, "dev")}),
+        ]
+
+    def _generate_examples(self, archive_path):
+        """Generate examples from a Librispeech archive_path."""
+        wav_dir = os.path.join(archive_path, "wav")
+        
+        for _id, c in enumerate(os.listdir(wav_dir)):
+            if os.path.isdir(f'{wav_dir}/{c}/'):
+                for file in os.listdir(f'{wav_dir}/{c}/'):
+                    if file.endswith('.wav'):
+                        wav_path = f'{wav_dir}/{c}/{file}'
+                        f = sf.SoundFile(wav_path)
+                        seconds = int(len(f)) // f.samplerate
+                        if seconds < 5 or seconds > 20:
+                            continue 
+                        example = {
+                            "file": wav_path,
+                            "label":c
+                        }
+                        yield str(_id), example
+
---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+python run_classifier.py \
+    --model_name_or_path="facebook/wav2vec2-large-xlsr-53" \
+    --output_dir=/content/model_dir/dialects \
+    --cache_dir=/content/data_dir/ \
+    --overwrite_output_dir \
+    --num_train_epochs="5" \
+    --per_device_train_batch_size="8" \
+    --preprocessing_num_workers="3" \
+    --learning_rate="3e-4" \
+    --warmup_steps="1" \
+    --evaluation_strategy="steps"\
+    --save_steps="1" \
+    --eval_steps="1" \
+    --save_total_limit="1" \
+    --logging_steps="1" \
+    --group_by_length \
+    --gradient_checkpointing \
+    --do_eval \
+    --do_train \
+    --max_train_samples 100 \
+    --max_val_samples 100 \
