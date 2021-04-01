b6d90db50c297c043e4007f123ef4286a3c317f9
zaidalyafeai <alyafey22@gmail.com>
2021-04-01 22:09:49 +0300

    add classes for recognition and classification

---
+++ klaam.py
@@ -0,0 +1,30 @@
+from transformers import (Wav2Vec2ForCTC,Wav2Vec2Processor)
+from .utils import load_file_to_data, predict
+from models import Wav2Vec2ClassificationModel
+from processor import CustomWav2Vec2Processor
+
+class SpeechRecognition:
+
+    def __init__(self, lang = 'egy', path = None):
+        if path is None:
+            model_dir = 'Zaid/wav2vec2-large-xlsr-53-arabic-egyptian'
+        else:
+            model_dir = path
+        self.model = Wav2Vec2ForCTC.from_pretrained(model_dir).to("cuda")
+        self.processor = Wav2Vec2Processor.from_pretrained(model_dir)
+    
+    def transcribe(self, wav_file):
+        return predict(load_file_to_data(wav_file), self.model, self.processor)
+
+class SpeechClassification:
+
+    def __init__(self, path = None):
+        if path is None:
+            dir = 'Zaid/wav2vec2-large-xlsr-arabic-classification'
+        else:
+            dir = path
+        self.model = Wav2Vec2ClassificationModel.from_pretrained(dir).to("cuda")
+        self.processor = CustomWav2Vec2Processor.from_pretrained(dir)
+    
+    def classify(self, wav_file):
+        return predict(load_file_to_data(wav_file), self.model, self.processor)
--- utils.py
+++ utils.py
@@ -1,19 +1,19 @@
 import soundfile as sf
 import torch
 
-def load_file_to_data(file):
-    batch = {}
-    start = 0 
-    stop = 20 
-    srate = 16_000
-    speech, sampling_rate = sf.read(file, start = start * srate , stop = stop * srate)
+def load_file_to_data(file, max_len = 20, srate = 16_000):
+    batch = {} 
+    speech, sampling_rate = sf.read(file, start = 0 , stop = max_len * srate)
     batch["speech"] = speech
     batch["sampling_rate"] = sampling_rate
     return batch
 
 
-def predict(data, processor, model):
-    features = processor(data["speech"], 
+def predict(data, processor, model, mode = 'rec'):
+    if 'rec':
+        features = processor(data["speech"], sampling_rate=data["sampling_rate"], padding=True, return_tensors="pt")
+    else:
+        features = processor(data["speech"], 
                         sampling_rate=data["sampling_rate"],
                         max_length=320000,
                         pad_to_multiple_of=320000,
@@ -23,5 +23,11 @@ def predict(data, processor, model):
     attention_mask = features.attention_mask.to("cuda")
     with torch.no_grad():
         outputs = model(**input_values, attention_mask=attention_mask)
-    pred_id = torch.argmax(outputs['logits'], dim=-1)[0]
-    return pred_id
+    
+    
+    if mode == 'rec':
+        pred_ids = torch.argmax(outputs.logits, dim=-1)[0]
+        return processor.batch_decode(pred_ids)
+    else:
+        pred_ids = torch.argmax(outputs['logits'], dim=-1)[0]
+        return pred_ids
